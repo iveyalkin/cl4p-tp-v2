16a0df852e34434fff92521e6019db63cdd76ea8
IV <op6yz@Basils-MacBook-Pro.local>
2016-11-02 12:21:37 -0400

    add me command

--- src/ClapTrap.cpp
+++ src/ClapTrap.cpp
@@ -5,24 +5,36 @@
 #include "ClapTrap.h"
 #include "stdio.h"
 
+#define CHAT_ID message->chat->id
+
 using namespace TgBot;
 using namespace ClapTp;
+
+std::string replaceMe(User::Ptr user) {
+    if (user->firstName.empty()) {
+        return user->username;
+    } else if (user->lastName.empty()) {
+        return user->firstName;
+    } else {
+        return std::string(user->firstName).append(" ").append(user->lastName);
+    }
+}
 
 ClapTrap::ClapTrap(char *token, SqlWrapper &sqlWrapper, std::string &logPrefix, std::string &debugChatId)
         : Bot(token), _dbInstance(sqlWrapper), _logPrefix(logPrefix), _debugChatId(debugChatId) {
 
     getEvents().onCommand("greeting", [this](Message::Ptr message) {
-        sendGreeting(message->chat->id);
+        sendGreeting(CHAT_ID);
     });
 
     getEvents().onCommand("querydb", [this](Message::Ptr message) {
         char buffer[1024 * 10];
         _dbInstance.fetchUrls(buffer);
-        getApi().sendMessage(message->chat->id, buffer);
+        getApi().sendMessage(CHAT_ID, buffer);
     });
 
     getEvents().onCommand("enough", [this](Message::Ptr message) {
-        replyToChat(message->chat->id,
+        replyToChat(CHAT_ID,
                     "Oh my God, I'm leaking! I think I'm leaking! Ahhhh, I'm leaking! There's oil everywhere!");
 
         _shoudlShutdown = true;
@@ -30,7 +42,7 @@ ClapTrap::ClapTrap(char *token, SqlWrapper &sqlWrapper, std::string &logPrefix, 
 
     getEvents().onCommand("echo", [this](Message::Ptr message) {
         std::string echoMsg = message->text.substr(strlen("/echo"));
-        replyToChat(message->chat->id,
+        replyToChat(CHAT_ID,
                     std::string("You hear an echo: ").append(echoMsg));
     });
 
@@ -49,13 +61,37 @@ ClapTrap::ClapTrap(char *token, SqlWrapper &sqlWrapper, std::string &logPrefix, 
         {
             reply = std::string("There is no quote in your message!");
         }
-        replyToChat(message->chat->id, reply);
+        replyToChat(CHAT_ID, reply);
     });
 
     getEvents().onCommand("unstash", [this](Message::Ptr message) {
         // TODO: load all message stored for message->from->id user
         // remove them from DB and send to chat
-        replyToChat(message->chat->id, "Did you really think I will store your junk!? Poor minion...");
+        replyToChat(CHAT_ID, "Did you really think I will store your junk!? Poor minion...");
+    });
+
+    getEvents().onCommand("me", [this](Message::Ptr message) {
+        std::vector<MessageEntity::Ptr>::const_iterator
+                itr = message->entities.begin(),
+                end = message->entities.end();
+        std::string messageText;
+        while (itr != end) {
+            if (!(*itr)->type.compare("bot_command")) {
+                if (!message->text.substr((*itr)->offset, (*itr)->length).compare("/me")) {
+                    std::string restText(message->text.substr((*itr)->length));
+                    if ((*itr)->offset > 0) {
+                        messageText = std::string(message->text.substr(0, (*itr)->offset));
+                    }
+                    messageText
+                            .append(replaceMe(message->from))
+                            .append(restText);
+
+                    replyToChat(CHAT_ID, messageText);
+                    return;
+                }
+            }
+            itr++;
+        }
     });
 
     getEvents().onAnyMessage([this](Message::Ptr message) {
